a489b1a1a09305b6ec11e63b7cfae51114443c42
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-07-08 23:12:57 -0400

    get current price with beautiful soup

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from bs4 import BeautifulSoup
+import lxml
 import requests
 
-product_url="https://www.amazon.com/Instant-Pot-Duo-Evo-Plus/dp/B07W55DDFB/ref=sr_1_1?qid=1597662463"
+product_url="https://www.amazon.com/2022-Apple-10-9-inch-Wi-Fi-256GB/dp/B09V3H8MJT/ref=sr_1_1_sspa?crid=2R5GUBO50N8M3&keywords=ipad&qid=1657335976&s=electronics&sprefix=ipad%2Celectronics%2C77&sr=1-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyMjZBOEFMVDVDODEmZW5jcnlwdGVkSWQ9QTA1NTU3ODAyMVhXMkhOTkJBUEI3JmVuY3J5cHRlZEFkSWQ9QTAxMzUzMzYyWjNQQUNRWFNGQ1FPJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=="
+
+headers={
+    "Accept-Language":"en-US,en;q=0.9",
+    "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+    }
 
 # Scrape Amazon
-response = requests.get(product_url)
-soup = BeautifulSoup(response.text, "html.parser")
-print(soup)
+response = requests.get(product_url,headers=headers)
+soup = BeautifulSoup(response.text, "lxml")
+price_whole = soup.find(name="span", class_="a-price-whole").getText()
+price_decimal = soup.find(name="span", class_="a-price-fraction").getText()
+price_concat = price_whole + price_decimal
+price = float(price_concat)
+print(price)
+
